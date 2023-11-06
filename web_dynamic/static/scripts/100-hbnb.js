621824c38d375f2f4898877310ed762ebd8cfab6
dominic-source <chinonsodomnic@gmail.com>
2023-11-05 17:22:05 -0800

    debug code and modify strategies

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -3,7 +3,8 @@ $(function () {
   const cities = {};
   const states = {};
 
-  $('li input[type=checkbox]').change(function () {
+  // Amenities listener
+  $('li input#amenity_filter').change(function () {
     if (this.checked) {
       amenities[this.dataset.name] = this.dataset.id;
     } else {
@@ -11,16 +12,41 @@ $(function () {
     }
     $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
   });
+
+
+  // State listener
+  $('li input#state_filter').change(function () {
+    if (this.checked) {
+      states[this.dataset.name] = this.dataset.id;
+    } else {
+      delete states[this.dataset.name];
+    }
+    $('.locations h4').text(Object.keys(states).sort().join(', '));
+  });
+
+
+  // city listener
+  $('li input#city_filter').change(function () {
+    if (this.checked) {
+      cities[this.dataset.name] = this.dataset.id;
+    } else {
+      delete cities[this.dataset.name];
+    }
+    $('.locations h4').text(Object.keys(cities).sort().join(', '));
+  });
+
+
   // Update with status on screen
   const url = 'http://0.0.0.0:5001/api/v1/status/';
-  $.get(url, function (data, status) {
-    if (status === 'OK') {
+  $.getJSON(url, function (data) {
+    if (data.status === 'OK') {
       $('div#api_status').addClass('available');
     } else {
       $('div#api_status').removeClass('available');
     }
   });
-  // Search places
+
+  // Search places, city, state
   const url2 = 'http://0.0.0.0:5001/api/v1/places_search/';
   const objData = {
     url: url2,
@@ -51,12 +77,14 @@ $(function () {
   };
   $.post(objData);
 
+
+  // Make the request when the search button is clicked
   $('section.filters button[type="button"]').on('click', function () {
-    objData.data = JSON.stringify({ amenities: Object.keys(amenities) });
+    objData.data = JSON.stringify({ 
+      amenities: Object.keys(amenities),
+      cities: Object.keys(cities),
+      states: Object.keys(states),
+    });
     $.post(objData);
   });
-  // Your code should be here, thanks
-
-
-
 });
